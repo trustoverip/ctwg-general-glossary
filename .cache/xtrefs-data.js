f511cc57f9228b0af402a634765a3f845528eab6
henkvancann <hvancann@bcws.io>
2025-08-29 08:32:47 +0200

    unpdated to beta-7
    Signed-off-by: henkvancann <hvancann@bcws.io>

---
+++ .cache/xtrefs-data.js
@@ -0,0 +1,56 @@
+const allXTrefs = {
+  "xtrefs": [
+    {
+      "externalSpec": "toip1",
+      "term": "decentralized-identifier",
+      "repoUrl": "https://github.com/henkvancann/ctwg-main-glossary",
+      "terms_dir": "spec/terms-definitions",
+      "owner": "henkvancann",
+      "repo": "ctwg-main-glossary",
+      "site": null,
+      "avatarUrl": null,
+      "ghPageUrl": "https://henkvancann.github.io/ctwg-main-glossary/",
+      "commitHash": "not found",
+      "content": "This term was not found in the external repository."
+    },
+    {
+      "externalSpec": "toip1",
+      "term": "decentralized-identity",
+      "repoUrl": "https://github.com/henkvancann/ctwg-main-glossary",
+      "terms_dir": "spec/terms-definitions",
+      "owner": "henkvancann",
+      "repo": "ctwg-main-glossary",
+      "site": null,
+      "avatarUrl": null,
+      "ghPageUrl": "https://henkvancann.github.io/ctwg-main-glossary/",
+      "commitHash": "not found",
+      "content": "This term was not found in the external repository."
+    },
+    {
+      "externalSpec": "keri1",
+      "term": "decentralized-identity",
+      "repoUrl": "https://github.com/henkvancann/kerisuite-glossary",
+      "terms_dir": "spec/terms-definitions",
+      "owner": "henkvancann",
+      "repo": "kerisuite-glossary",
+      "site": null,
+      "avatarUrl": null,
+      "ghPageUrl": "https://henkvancann.github.io/kerisuite-glossary/",
+      "commitHash": "6820014a1111b38dc3d653a0026ff981db2dee17",
+      "content": "<dd>\n<p><code>KERI</code>s definition of decentralization (centralization) is about <em>control</em> not <em>spatial distribution</em>. In our definition <em>decentralized</em> is not necessarily the same as <em>distributed</em>. By distributed we mean that activity happens at more than one site. Thus decentralization is about <em>control</em> and distribution is about <em>place</em>. To elaborate, when we refer to decentralized infrastructure we mean infrastructure under decentralized (centralized) control no matter its spatial distribution. Thus <em>decentralized infrastructure</em> is infrastructure sourced or controlled by more than one <code>entity</code>.</p>\n</dd>\n<dd>\n<p>TrustoverIP related <a class=\"x-term-reference term-reference\" data-local-href=\"#term:toip1:decentralized-identity\" href=\"https://henkvancann.github.io/ctwg-main-glossary/#term:decentralized-identity\">decentralized-identity</a></p>\n</dd>\n<dd>\n<p>General IT description <a class=\"x-term-reference term-reference\" data-local-href=\"#term:toip2:decentralized-identity\" href=\"https://henkvancann.github.io/ctwg-general-glossary/#term:decentralized-identity\">decentralized-identity</a></p>\n</dd>"
+    },
+    {
+      "externalSpec": "toip1",
+      "term": "User Datagram Protocol",
+      "repoUrl": "https://github.com/henkvancann/ctwg-main-glossary",
+      "terms_dir": "spec/terms-definitions",
+      "owner": "henkvancann",
+      "repo": "ctwg-main-glossary",
+      "site": null,
+      "avatarUrl": null,
+      "ghPageUrl": "https://henkvancann.github.io/ctwg-main-glossary/",
+      "commitHash": "6de0480a63aaf618e786e165a584a1a1b63bc7d0",
+      "content": "<dd>\n<p>In computer networking, the User Datagram Protocol (UDP) is one of the core <a class=\"term-reference\" href=\"#term:communication\">communication</a> protocols of the <a class=\"term-reference\" href=\"#term:internet-protocol-suite\">Internet protocol suite</a> used to send <a class=\"term-reference\" href=\"#term:messages\">messages</a> (transported as <a class=\"term-reference\" href=\"#term:datagrams\">datagrams</a> in <a class=\"term-reference\" href=\"#term:packets\">packets</a>) to other hosts on an <a class=\"term-reference\" href=\"#term:internet-protocol\">Internet Protocol</a> (IP) network. Within an IP network, UDP does not require prior communication to set up <a class=\"term-reference\" href=\"#term:communication-channels\">communication channels</a> or data paths.</p>\n</dd>\n<dd>\n<p>Source: <a path-0=\"en.wikipedia.org\" path-1=\"wiki\" path-2=\"User_Datagram_Protocol\" href=\"https://en.wikipedia.org/wiki/User_Datagram_Protocol\">Wikipedia</a>.</p>\n</dd>\n<dd>\n<p>Also known as: <a class=\"term-reference\" href=\"#term:udp\">UDP</a>.</p>\n</dd>"
+    }
+  ]
+};
